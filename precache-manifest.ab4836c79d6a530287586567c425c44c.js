e3c06f2794d73d771280605252fd32f4dc91d715
marc.blumberg <marcaugustblumberg@gmail.com>
2020-06-01 10:37:44 -0700

    Updates

---
+++ precache-manifest.ab4836c79d6a530287586567c425c44c.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/bip-mobile/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "6132aa6520c7e06c4295",
+    "url": "/bip-mobile/static/js/app.bc8770f5.chunk.js"
+  },
+  {
+    "revision": "522bee4a69c1257e69d7ccdfe6a0ab2a",
+    "url": "/bip-mobile/static/media/circle_PNG4.522bee4a.png"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/bip-mobile/static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/bip-mobile/static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/bip-mobile/static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/bip-mobile/static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "d08d37ed3dde5a23fc86cfec323c0fa2",
+    "url": "/bip-mobile/static/media/circle_PNG3.d08d37ed.png"
+  },
+  {
+    "revision": "4c461aef252251a31e91260ac0b98e73",
+    "url": "/bip-mobile/static/media/circle_PNG6.4c461aef.png"
+  },
+  {
+    "revision": "b23e4fabad55ce1c3cf2",
+    "url": "/bip-mobile/static/js/runtime~app.aab2bbc0.js"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/bip-mobile/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "771e5f5f15c66a9b046aa0159989d506",
+    "url": "/bip-mobile/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/bip-mobile/serve.json"
+  },
+  {
+    "revision": "79bd51b8bf26a83655b6",
+    "url": "/bip-mobile/static/js/2.01ca65b0.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/bip-mobile/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/bip-mobile/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "12dea3ac686099278fd3d022bb7dc8ad",
+    "url": "/bip-mobile/manifest.json"
+  },
+  {
+    "revision": "baea4d0295ba3cdb0e9f002f2e0145ae",
+    "url": "/bip-mobile/index.html"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/bip-mobile/favicon.ico"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/bip-mobile/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/bip-mobile/favicon-32.png"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/bip-mobile/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/bip-mobile/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/bip-mobile/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/bip-mobile/favicon-16.png"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/bip-mobile/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/bip-mobile/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/bip-mobile/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/bip-mobile/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/bip-mobile/./fonts/Entypo.ttf"
+  }
+];
