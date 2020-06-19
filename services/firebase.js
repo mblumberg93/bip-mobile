f33d550b30b5a6a2e100aafa48e10e7e6f4b38ae
marc.blumberg <marcaugustblumberg@gmail.com>
2020-06-19 13:45:02 -0700

    Removed firebase analytics

--- services/firebase.js
+++ services/firebase.js
@@ -1,8 +1,6 @@
 import * as firebase from 'firebase';
-import 'firebase/analytics';
 import { FIREBASE_CONFIG } from '../secrets';
 
 firebase.initializeApp(FIREBASE_CONFIG);
-firebase.analytics();
 
 export const firebaseDB = firebase.database();
